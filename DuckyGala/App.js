d7b311a6e5df4038ef04fd9d318e619d4dc79035
HoangTung108 <warbat082003@gmail.com>
2024-04-27 08:07:42 +0700

    Update main

--- DuckyGala/App.js
+++ DuckyGala/App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const[fontsLoaded,fontError] = useFonts ({
+    "Pacifico" : require("./assets/fonts/Pacifico-Regular.ttf"),
+  });
+
   return (
     <View style={styles.container}>
-      <Text>DuckyGalo</Text>
-      <StatusBar style="auto" />
+      <Text style = {styles.header}>DuckyGala</Text>
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +21,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'top',
-    
   },
+    header:{
+      color: "#fff",
+      backgroundColor: "#000",
+      fontFamily: "Pacifico",
+      fontSize: 40,
+    },
 });
