5a2ee2ccef9584164ba28ce72e1ea11c61bb3fa0
HoangTung108 <warbat082003@gmail.com>
2024-04-29 08:35:23 +0700

    Update main

--- DuckyGala/App.js
+++ DuckyGala/App.js
@@ -1,22 +1,27 @@
 import React from 'react-native';
 import { StyleSheet,TextInput, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
+import { useState } from 'react-native';
 
 
 export default function App() {
   const[fontsLoaded,fontError] = useFonts ({
     "Pacifico" : require("./assets/fonts/Pacifico-Regular.ttf"),
   });
+  const [text,setText] = useState ('');
   return (
     <View style={styles.container}>
       <Text style = {styles.header}>DuckyGala</Text>
-
-
+      <Text>Your Spend: {text}</Text>
+      <TextInput style = {styles.inputText}
+      placeholder=' Cash here'
+      onChangeText={ newText => setText (newText)}
+      defaultValue= {text}
+      />
     </View>
   );
 
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
