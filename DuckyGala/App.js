8fdfc478276ee882a80cdbefbdfb3b36f46b51a3
HoangTung108 <warbat082003@gmail.com>
2024-05-12 10:43:19 +0700

    Update main

--- DuckyGala/App.js
+++ DuckyGala/App.js
@@ -1,28 +1,12 @@
-import React, {useState} from 'react';
-import { View, Text, Switch} from 'react-native';
+import React from 'react';
+import { View, Text} from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import {HomeScreen} from './Screen/Home'
 import { Setting } from './Screen/Setting';
 
-function SecondScreen() {
-    const [isEnabled, setIsEnabled] = React.useState(false);
-    const toggleSwitch = () => {
-    setIsEnabled((previousState) => !previousState);
-  };
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Switch
-        trackColor={{ false: '#767577', true: '#81b0ff' }}
-        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
-    </View>
-  );
-}
+
 function ScreenView(){
   return(
     <View>
